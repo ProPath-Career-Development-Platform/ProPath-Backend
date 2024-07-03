807cdb2eb4899d842b64aadb317c6ca75c208952
Pahasara Jayasuriya <136761380+Pahasarajayasuriya@users.noreply.github.com>
2024-07-03 17:57:41 +0530

    first commit

---
+++ src/main/java/Propath/ProPathApplication.java
@@ -0,0 +1,13 @@
+package Propath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProPathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Propath/ProPathApplicationTests.java
@@ -0,0 +1,13 @@
+package Propath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
