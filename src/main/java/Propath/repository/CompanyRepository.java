e9d48812ec06956e85df03647f98d09e516c2f08
Tharindu-Liyanage <tharinduprabashwara71@gmail.com>
2024-09-05 16:12:02 +0530

    Update JobProvider Job
    -update job
    -update company

--- src/main/java/Propath/repository/CompanyRepository.java
+++ src/main/java/Propath/repository/CompanyRepository.java
@@ -3,8 +3,10 @@ package Propath.repository;
 import Propath.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CompanyRepository extends JpaRepository<Company,Long> {
-    Company findByUserId(int user_id);
+    Optional<Company> findByUserIdAndStatus(int user_id,String text);
 
 
 
