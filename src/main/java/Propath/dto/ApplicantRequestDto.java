26f7fd5675c7cb58ed82344be0405787ee8d2e1c
Tharindu-Liyanage <tharinduprabashwara71@gmail.com>
2024-11-25 12:37:01 +0530

    Update Final Candidate List

---
+++ src/main/java/Propath/dto/ApplicantRequestDto.java
@@ -0,0 +1,18 @@
+package Propath.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplicantRequestDto {
+    private List<Integer> ids;
+    private Long jobId;
+}
+
